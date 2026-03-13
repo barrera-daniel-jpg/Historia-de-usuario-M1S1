a8b6c087e667616a0871aabc443e5f7a48353dea
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-13 10:34:22 -0500

    comentarios

--- inventory.py
+++ inventory.py
@@ -1,11 +1,14 @@
 def inicio():
-    print("===========================") #Linea que mejora la experiencia del sistema
+    print("===========================") #Linea que mejora la experiencia del usuario de forma visual
     print(">> BIENVENIDO AL SISTEMA <<") # Mensaje de bienvenida al usuario
     print("===========================") 
 #Funcion encargada de darle la bienvenida al usuario
 
-def productos():
-    producto = input("\n>> Ingrese el nombre del producto: ").capitalize()
+def productos(): #Funcion encargada de registrar las caracteristicas del producto
+
+    producto = input("\n>> Ingrese el nombre del producto: ").capitalize() #Le pide al usuario que ingrese el nombre del producto
+    
+    # Evular los posibles errores que se pueden presentar
     try:
         price = float(input(">> Ingrese el precio del producto: $"))
         quantity = int(input(">> Ingrese la cantidad vendida del producto: "))
@@ -13,18 +16,17 @@ def productos():
         print("-"*40)
         print("Datos invalidos, vuelva intentarlo.")
         print("-"*40)
-        return productos()
-    costo_total= price*quantity
+        return productos() # Sirve para retornar la funcion dado caso se presente un error al ingresar un dato
+    costo_total= price*quantity # Calcula el costo total del precio por la cantidad del producto
     
-    print("-"*60)
+    print("-"*60) #Linea que mejora la experiencia del usuario de forma visual
     print (f"Producto = {producto} | Precio = ${price} | Cantidad = {quantity} | Total = ${costo_total}")
+    # Imprime el resumen de los datos agregados tales como: nombre, precio, cantidad y costo total del producto
     print("-"*60)
     
-#Funcion encargada de registrar las caracteristicas del producto
-def run():
-    inicio()
-    productos()
-#Funcion encargada de ejecutar todas las funciones anteriores
+def run(): #Funcion encargada de ejecutar todas las funciones anteriores
 
-run()
-#Ejecuta la funcion nombrada para iniciar el programa
+    inicio() #Llama dicha funcion para ejecutarse
+    productos() #Llama dicha funcion para ejecutarse
+
+run() #Ejecuta la funcion nombrada para iniciar el programa.
